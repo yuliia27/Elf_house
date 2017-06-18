f1a4551c0511e2d6012916970a34577a58aaa82b
yuliia27 <yuliia.panina@gmail.com>
2017-06-18 23:31:09 +0300

    Update SundayEvening.java

--- SundayEvening.java
+++ SundayEvening.java
@@ -1,7 +1,7 @@
 public class SundayEvening{
 
     public static void main(String[] args) {	
-        System.out.println("Hello, Sunday Evening!!!");
+        System.out.println("Hello, Sunday Afternoon!!!");
     }
  
 }
